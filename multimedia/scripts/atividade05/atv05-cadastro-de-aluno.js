13250553da40ac8f9366c097469fb72dd43a867e
Ian Santos <iann.silva2006@gmail.com>
2023-09-23 16:48:03 -0300

    new activity

---
+++ multimedia/scripts/atividade05/atv05-cadastro-de-aluno.js
@@ -0,0 +1,41 @@
+const matricula_input = document.querySelector("#txt-matricula");
+const nome_input = document.querySelector("#txt-nome");
+const email_input = document.querySelector("#txt-email");
+
+const salvar_exibir_BTN = document.querySelector("#salvar-exibir");
+const atualizar_BTN = document.querySelector("#atualizar");
+const excluir_BTN = document.querySelector("#excluir");
+const Aluno = {};
+
+salvar_exibir_BTN.addEventListener("click", () => {
+    if (salvar_exibir_BTN.innerText === "Exibir") {
+        matricula_input.value = Aluno.matricula;
+        nome_input.value = Aluno.nome;
+        email_input.value = Aluno.email;
+    }
+});
+
+excluir_BTN.addEventListener("click", () => {
+    for (const propriedade in Aluno) {
+        delete Aluno[propriedade];
+    };
+    salvar_exibir_BTN.innerText = "Salvar";
+    salvar_exibir_BTN.type = "submit";
+});
+
+function atualizar_cadastro() {
+    Aluno.matricula = matricula_input.value;
+    Aluno.nome = nome_input.value;
+    Aluno.email = email_input.value;
+    limpar_input();
+    if (salvar_exibir_BTN.innerText === "Salvar") {
+        salvar_exibir_BTN.innerText = "Exibir";
+        salvar_exibir_BTN.type = "button";
+    }
+}
+
+function limpar_input() {
+    matricula_input.value = "";
+    nome_input.value = "";
+    email_input.value = "";
+}
