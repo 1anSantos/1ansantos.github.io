60d8545fd849fa64533a87558c718f460c8a8d40
Ian Santos <iann.silva2006@gmail.com>
2023-10-05 23:35:13 -0300

    atv09 separar-nome-sobrenome completed

---
+++ scripts/atividade09/atv09-separar-nome-sobrenome.js
@@ -0,0 +1,20 @@
+// inputs
+const nomeCompleto = document.querySelector("#nome-completo");
+const sobrenome = document.querySelector("#sobrenome");
+const nome = document.querySelector("#nome");
+
+// botoes
+const separar = document.querySelector("#separar");
+const juntar = document.querySelector("#juntar");
+
+separar.addEventListener("click", () => {
+    const txt = nomeCompleto.value.trim().replace(/ +/g, ' ');
+    const lista = txt.split(' ');
+    nome.value = lista[0];
+    sobrenome.value = lista[1] || '';
+});
+
+juntar.addEventListener("click", () => {
+    const lista = [nome.value, sobrenome.value];
+    nomeCompleto.value = lista.join(' ').trim();
+});
