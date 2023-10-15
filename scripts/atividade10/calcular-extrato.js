c49c84b905c9c62dbe3b9a168112ace6074a93e9
Ian Santos <iann.silva2006@gmail.com>
2023-10-15 01:28:09 -0300

    extrato update

--- scripts/atividade10/calcular-extrato.js
+++ scripts/atividade10/calcular-extrato.js
@@ -1,91 +1,243 @@
-// Tabela
-const tabela = document.getElementById("extrato-linhas");
-
-// Extrato
-const extrato = document.getElementById("extrato-resumo");
-const extratoTotal = document.getElementById("extrato-total");
-const extratoMeses = document.getElementById("extrato-meses");
-const extratoMercadoTotal = document.getElementById("extrato-total-mercado");
-const extratoMercadoMaior = document.getElementById("extrato-maior-gasto-mercado");
-
 // Inputs
-const categoria = document.getElementById("categoria");
-const valor = document.getElementById("valor");
-const data = document.getElementById("data");
+const categoria = document.querySelector("#categoria");
+const valor = document.querySelector("#valor");
+const error = document.querySelector("#error");
+const data = document.querySelector("#data");
 
-// Variáveis
-const gastos = [];
+// Tabela
+const tabela = document.querySelector("#gastos-tabela");
 
-function adicionar() {
-    const dataValue = data.value.replace(/-/g, "/");
-    const valorValue = valor.value.replace(",", ".");
+// Form
+const formInputs = document.querySelector("#form-inputs");
 
-    if (dataValue === "" || valorValue === "") return;
-    if (isNaN(valorValue)) {
-        alert("Coloque um valor válido!");
-        return;
+// Buttons
+const btnExemplo = document.querySelector("#exemplo");
+const btnCalcular = document.querySelector("#calcular");
+const btnLimparExtrato = document.querySelector("#limpar-extrato");
+
+// Resumo
+const resumo = document.querySelector("#resumo");
+const totalGasto = document.querySelector("#total-gasto");
+const maiorGasto = document.querySelector("#maior-gasto");
+const totalData = document.querySelector("#total-data");
+const totalCategoria = document.querySelector("#total-categoria");
+const maiorCategoria = document.querySelector("#maior-categoria");
+
+// Classes
+class Gasto {
+    static _totalGasto = 0;
+    static _listaGastos = [];
+    constructor(categoria, data, valor) {
+        this.categoria = categoria;
+        this.data = data;
+        this.valor = valor;
     }
+    static getTotalGasto() {
+        Gasto.atualizarTotalGasto();
+        return Gasto._totalGasto;
+    }
+    static atualizarTotalGasto() {
+        Gasto._totalGasto = 0;
+        Gasto._listaGastos.forEach ((gasto) => {
+            Gasto._totalGasto += gasto.valor;
+        });
+    }
+    static getLista() {
+        return Gasto._listaGastos;
+    }
+    static limparLista() {
+        Gasto._listaGastos.splice(0);
+        Gasto._totalGasto = 0;
+    }
+    static adicionarALista(gasto) {
+        if (!Gasto.validarGasto(gasto)) {
+            console.error("Validação deu errado");
+            return;
+        }
+        Gasto._listaGastos.push(gasto);
+        Gasto.atualizarTotalGasto();
+        Gasto._listaGastos.sort((a, b) => b.data.localeCompare(a.data));
+    }
+    static validarGasto(gasto) {
+        if (Object.keys(gasto).length > 3) return false;
+        if (typeof gasto.categoria !== "string") return false;
+        if (typeof gasto.valor !== "number") return false;
+        if (typeof gasto.data !== "string") return false;
+        if (gasto.valor < 0) return false;
+        // Validar Data
+        const date = new Date(gasto.data);
+        if (date.toISOString().slice(0, 10) !== gasto.data) return false;
 
-    gastos.push({
-        categoria: categoria.value,
-        data: dataValue,
-        valor: Number(valorValue)
-    });
-    tabela.innerHTML += `
-    <tr>
-        <td>${categoria.value}</td>
-        <td>${dataValue}</td>
-        <td>${valorValue}</td>
-    </tr>
-    `
+        return true;
+    }
 }
 
-function calcular() {
-    if (tabela.children.length === 1) {
-        alert("Nada para calcular aqui");
+// Event Listeners
+formInputs.addEventListener("submit", adicionarValores);
+
+btnExemplo.addEventListener("click", () => {
+    const lista = [
+        new Gasto("Viagem", "2022-09-08", 368.95),
+        new Gasto("Eventos", "2023-02-25", 50),
+        new Gasto("Transporte", "2022-09-03", 80.6),
+        new Gasto("Mercado", "2023-02-20", 10),
+        new Gasto("Mercado", "2023-03-09", 40),
+        new Gasto("Viagem", "2022-06-04", 1502.80),
+        new Gasto("Transporte", "2023-06-30", 143.70),
+        new Gasto("Eventos", "2022-02-14", 50)
+    ];
+    lista.forEach((obj) => Gasto.adicionarALista(obj));
+    atualizarTabela();
+});
+
+btnCalcular.addEventListener("click", (event) => {
+    atualizarResumo();
+    event.preventDefault();
+});
+
+btnLimparExtrato.addEventListener("click", (event) => {
+    limparDados();
+    event.preventDefault();
+});
+
+// Functions
+function limparCampo() {
+    categoria.selectedIndex = 0;
+    valor.value = "";
+    data.value = "";
+}
+
+function limparDados() {
+    limparCampo();
+    Gasto.limparLista();
+    atualizarTabela();
+    resumo.style.display = "none";
+}
+
+function adicionarValores() {
+    const valorNumber = Number(valor.value.replace(",", "."));
+    if (verificarErro()) return;
+
+    const novoGasto = new Gasto(categoria.value, data.value, valorNumber);
+    Gasto.adicionarALista(novoGasto);
+    atualizarTabela();
+    limparCampo();
+    valor.removeEventListener("input", adicionarValores);
+}
+
+function atualizarTabela() {
+    tabela.innerHTML = "";
+    const lista = Gasto.getLista();
+
+    for (const gasto of lista) {
+        const dataFormatada = [...gasto.data.split("-")].reverse().join("/");
+        tabela.innerHTML += `
+            <tr>
+                <td>${gasto.categoria}</td>
+                <td>${dataFormatada}</td>
+                <td>${gasto.valor}</td>
+            </tr>
+        `
+    }
+}
+
+function verificarErro() {
+    const valorNumber = Number(valor.value.replace(",", "."));
+    if (isNaN(valorNumber)) {
+        error.style.display = "block";
+        error.innerText = "* Só aceitamos números!";
+        valor.addEventListener("input", verificarErro);
+        return true;
+    }
+    if (valorNumber < 0) {
+        error.style.display = "block";
+        error.innerText = "* Só aceitamos números positivos!";
+        valor.addEventListener("input", verificarErro);
+        return true;
+    }
+    error.style.display = "none";
+    return false;
+}
+
+function atualizarResumo() {
+    if (tabela.innerHTML === "") {
+        alert("Nada para resumir");
         return;
     }
-    if (extrato.style.display === "none") {
-        extrato.style.display = "block";
-    }
+    totalGasto.innerText = Gasto.getTotalGasto().toFixed(2);
+    maiorGasto.innerText = Math.max(...Gasto.getLista().map((e) => e.valor)).toFixed(2);
+    const lista = Gasto.getLista();
+    atualizarResumoTotalGastoDatas(lista);
+    atualizarResumoTotalGastoCategorias(lista);
+    atualizarResumoMaiorGastoCategorias(lista);
+    resumo.style.display = "block";
+}
 
-    gastos.sort(mesesEmOrdemCrescente);
+function atualizarResumoMaiorGastoCategorias(lista) {
+    const listaCategoria = [...lista].sort((a,b) => a.categoria.localeCompare(b.categoria));
+    maiorCategoria.innerHTML = "<hr>"; 
+    let atualGasto = 0;
+    let categoria;
 
-    extratoTotal.innerText = 'Total: ' + gastos.reduce((sum, gasto) => sum + gasto.valor, 0);
-    extratoMercadoMaior.innerText = 'Maior gasto com Mercado: ' + maiorGasto();
-    extratoMercadoTotal.innerText = 'Total de gastos com Mercado: ' + mercadoTotal();
-    let mes;
-    extratoMeses.innerHTML = "";
-    for (let i = 0; i < gastos.length; i++) {
-        let mesGasto = gastos[i].data.split("/")[1];
-        if (mesGasto === mes) {
-            const p = extratoMeses.children[extratoMeses.children.length - 1];
-            const valorAntigo = Number(p.innerText.split(":")[1].replace("</p>", ""));
-            const novoValor = valorAntigo + gastos[i].valor;
-            p.innerHTML = `<p>Total no Mês ${mesGasto}: ${novoValor}</p>`
+    for (const gasto of listaCategoria) {
+        if (gasto.categoria !== categoria) {
+            maiorCategoria.innerHTML += `<p>Maior gasto com ${gasto.categoria}: <span class="valor">${gasto.valor.toFixed(2)}</span></p>`;
+            atualGasto = gasto.valor;
+            categoria = gasto.categoria;
         }
         else {
-            mes = mesGasto;
-            extratoMeses.innerHTML += `<p>Total no Mês ${mesGasto}: ${gastos[i].valor}</p>`
+            const valores = maiorCategoria.querySelectorAll(".valor");
+            const atualValor = valores[valores.length - 1];
+            atualGasto = Math.max(atualGasto, gasto.valor);
+            atualValor.innerText = atualGasto.toFixed(2);
         }
     }
 }
 
-function mesesEmOrdemCrescente(a, b) {
-    const mes1 = a.data.split("/")[1];
-    const mes2 = b.data.split("/")[1];
-    return mes1 - mes2;
+function atualizarResumoTotalGastoCategorias(lista) {
+    const listaCategoria = [...lista].sort((a,b) => a.categoria.localeCompare(b.categoria));
+    totalCategoria.innerHTML = "<hr>"; 
+    let categoria;
+
+    for (const gasto of listaCategoria) {
+        if (gasto.categoria !== categoria) {
+            totalCategoria.innerHTML += `<p>Total de gastos com ${gasto.categoria}: <span class="valor">${gasto.valor.toFixed(2)}</span></p>`;
+            categoria = gasto.categoria;
+        }
+        else {
+            const valores = totalCategoria.querySelectorAll(".valor");
+            const atualValor = valores[valores.length - 1];
+            const soma = Number(atualValor.innerText) + gasto.valor;
+            atualValor.innerText = soma.toFixed(2);
+        }
+    }
 }
 
-function maiorGasto() {
-    const listaMercado = gastos.filter(gasto => gasto.categoria === "Mercado");
-    let valores = listaMercado.map(gasto => gasto.valor);
-    if (valores.length === 0) valores = [0,0];
-    return Math.max(...valores);
-}
-
-function mercadoTotal() {
-    const listaMercado = gastos.filter(gasto => gasto.categoria === "Mercado");
-    const totalGastos = listaMercado.reduce((sum, gasto) => sum + gasto.valor, 0);
-    return totalGastos;
+function atualizarResumoTotalGastoDatas(lista) {
+    let ano;
+    let mes;
+    totalData.innerHTML = "";
+    for (const gasto of lista) {
+        const anoGasto = gasto.data.split("-")[0];
+        const mesGasto = gasto.data.split("-")[1];
+        if (ano !== anoGasto) {
+            totalData.innerHTML += `
+            <h3>${anoGasto}</h3>
+            <hr>
+            <p>Total no Mês ${mesGasto}: <span class="valor">${gasto.valor.toFixed(2)}</span></p>
+            `;
+            ano = anoGasto;
+            mes = mesGasto;
+        }
+        else if (mes !== mesGasto) {
+            totalData.innerHTML += `<p>Total no Mês ${mesGasto}: <span class="valor">${gasto.valor.toFixed(2)}</span></p>`;
+            mes = mesGasto;
+        }
+        else {
+            const valores = totalData.querySelectorAll(".valor");
+            const atualValor = valores[valores.length - 1];
+            const soma = Number(atualValor.innerText) + gasto.valor
+            atualValor.innerText = soma.toFixed(2);
+        }
+    }
 }
